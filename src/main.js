3f398af6c0f44d63c59d01318556b53e50ef156a
jurgel <agustinus.arya@gmail.com>
2021-12-19 02:30:16 +0700

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import './index.css'
-
-const app = createApp(App)
-app.use(VueAxios, axios)
-app.mount('#app')
