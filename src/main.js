526169f53a8a521917e1780e98f424446d80168c
jurgel <agustinus.arya@gmail.com>
2021-12-19 09:39:21 +0700

    add gtag

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueGtag from 'vue-gtag-next'
 import './index.css'
 
 const app = createApp(App)
+
 app.use(VueAxios, axios)
+app.use(VueGtag, {
+  property: {
+    id: 'G-5412NGB8K4',
+    params: {
+      send_page_view: true,
+    },
+  },
+})
+
 app.mount('#app')
